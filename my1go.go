4ef497915a1d72fb1a7b8326f5ae86d8d4bbbd48
Alexander Bolshakov <bolshaaan@gmail.com>
2016-08-05 12:52:00 +0300

    read

--- my1go.go
+++ my1go.go
@@ -6,17 +6,22 @@ import (
 	"fmt"
 	"flag"
 	"./ya-money-go/"
+	"io/ioutil"
 )
 
 func main() {
 	fmt.Println("hello world :)))")
 
-	wordPtr := flag.String("word", "foo123", "a string")
-
+	file_name := flag.String("token_file", "/home/alexander/token_file", "file, containing yamoney token" )
 	flag.Parse()
-	fmt.Println("word:", *wordPtr)
 
-	ya, _ := yamoney.NewYaMoney("410011329603013.1C6223286B618EBF7A24C3EB9EC7D1C6E243789EBBEAD6555D2FB55384C82B0A11614E8F576679C6CB057C9F753716788E7B9F3755753D3BE1BFF1D5369D81FEC1970B5DCBE090BF25F22577CEFDF9F83BA2882763D6AD26E54D41CC31D568B6B5E7E3658AE443805F58AE9BE602C588B7896DF918DE20BFE51638C92555F8AC")
+	token, err := ioutil.ReadFile(*file_name)
+
+	if err != nil {
+		panic(err)
+	}
+
+	ya, _ := yamoney.NewYaMoney(string(token))
 
 	// Start period
 	from, _ := time.Parse( "2006-Jan-02 00:00:00", "2016-Aug-01 00:00:00" )
